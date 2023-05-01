7429147eb111487aa4ddb8b07dcec74c334da278
Nikita Romanov <nikita.romanov@devexpress.com>
2023-05-01 19:23:48 +0400

    icon

--- src/VSCode.Extension/extension.ts
+++ src/VSCode.Extension/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 	
 		const project = e.configuration.selected_project;
 		const device = e.configuration.selected_device;
-		const framework = project.frameworks?.find((it: any) => it.includes(device.platform))
+		const framework = project.frameworks.find((it: any) => it.includes(device.platform))
 		HotReloadController.startHotReload(project.path, framework);
 	}));
 	context.subscriptions.push(vscode.debug.onDidTerminateDebugSession((e) => {
